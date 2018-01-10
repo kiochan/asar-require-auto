f66b0ee198ac39bdc5b78187765e4799ba8b3674
Kiochan <sunxingchen@live.com>
2018-01-10 07:30:26 +0100

    delete unnecessary code

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 function loadFromAsar (path_str) {
   let _json = require(path_str + '/package.json')
-  let _module = require(path.resolve(path_str + '/package.json', _json.main))
+  let _module = require(
+    path.resolve(path_str + '/package.json', _json.main)
+  )
   return _module
 }
 
@@ -11,20 +13,12 @@ function asarRequireAuto (path_str) {
   let _module = null
 
   if (path_str.match(/\.asar$/)) {
-    return loadFromAsar(path_str)
+    _module = loadFromAsar(path_str)
+  } else {
+    _module = require(path_str)
   }
 
-  try {
-    _module = loadFromAsar(path_str)
-    return _module
-  } catch (errAsar) {
-    try {
-      _module = require(path_str)
-      return _module
-    } catch (err) {
-      throw err
-    }
-  }
+  return _module
 }
 
 module.exports = asarRequireAuto
