e28fca9e699459d59819632052f7a95e2f051c29
Kiochan <sunxingchen@live.com>
2018-01-10 07:14:36 +0100

    fix path.resolve bug

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 require('asar-require')
+const path = require('path')
 
-function loadFromAsar (path) {
-  let _json = require(path + '/package.json')
-  let _module = require(path.resolve(path + '/package.json', _json.main))
+function loadFromAsar (path_str) {
+  let _json = require(path_str + '/package.json')
+  let _module = require(path.resolve(path_str + '/package.json', _json.main))
   return _module
 }
 
-function asarRequireAuto (path) {
+function asarRequireAuto (path_str) {
   let _module = null
 
-  if (path.match(/\.asar$/)) {
-    return loadFromAsar(path)
+  if (path_str.match(/\.asar$/)) {
+    return loadFromAsar(path_str)
   }
 
   try {
-    _module = loadFromAsar(path)
+    _module = loadFromAsar(path_str)
     return _module
   } catch (errAsar) {
     try {
-      _module = require(path)
+      _module = require(path_str)
       return _module
     } catch (err) {
       throw err
